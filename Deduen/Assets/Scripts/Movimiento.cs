66e33e0171bba3baf32c59612729d4fb1b591378
marrazkilari <martin_leoz@euneiz.com>
2026-02-17 17:09:01 +0100

    MovimientoHecho

--- Deduen/Assets/Scripts/Movimiento.cs
+++ Deduen/Assets/Scripts/Movimiento.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //EVITAR DIAGONALES
+        //if (horizontal != 0)
+        //{
+        //    vertical = 0;
+        //}
+
+        movement = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * speed;
     }
 }
