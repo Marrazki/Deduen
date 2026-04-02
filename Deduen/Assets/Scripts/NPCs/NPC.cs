278189ca08195d087473a928e28eea2a2ffeb4c1
marrazki <martin_leoz@euneiz.com>
2026-04-02 12:48:40 +0200

    ClaseNPC

---
+++ Deduen/Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NPC
+{
+    private string name;
+    private int id;
+    private string[] dialogos = new string[100];
+
+    public void Hablar()
+    {
+        // Aquí puedes implementar la lógica para que el NPC hable, por ejemplo, mostrando un diálogo en pantalla.
+    }
+}
