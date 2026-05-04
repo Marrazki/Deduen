f586de1b7aad675003d6c4e6213192cb66613bc6
Marrazki <martin_leoz@euneiz.com>
2026-05-04 11:55:40 +0200

    Inventario_1.1

--- Deduen/Assets/NightRunStudio_Inventario/Scripts/InventoryManager.cs
+++ Deduen/Assets/NightRunStudio_Inventario/Scripts/InventoryManager.cs
@@ -1,7 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject InvenoryMenu;
+    private bool menuActivated;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,17 @@ public class InventoryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            //Time.timeScale = 1f;  //Reanuda el tiempo
+            InvenoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            //Time.timeScale = 0f;  //Para el tiempo
+            InvenoryMenu.SetActive(true);
+            menuActivated = true;
+        }
     }
 }
